fix: the precision@k early stop

mainTopKBatchDiffNeg.py
@@ -124,14 +124,14 @@ for epoch in range(world.TRAIN_epochs):
         )
         if "NNI_PLATFORM" in os.environ:
             metric = {
-                "default": valid_ndcg[0],
-                "recall": valid_recall[0],
+                "ndcg": valid_ndcg[0],
+                "default": valid_recall[0],
                 "hit": valid_hit[0],
                 "precision": valid_precision[0],
             }
             nni.report_intermediate_result(metric)
 
-        if valid_ndcg[0] > best_ndcg[0] + 0.0001:
+        if valid_recall[0] > best_recall[0] + 0.0001:
             patience = 0
             if "NNI_PLATFORM" not in os.environ:
                 torch.save(Recmodel.state_dict(), os.path.join(save_dir, "best_model.pth"))
@@ -144,7 +144,7 @@ for epoch in range(world.TRAIN_epochs):
                 print("Early stop!")
                 cprint('[Test]')
                 test_res = procedure.Test(dataset, best_Recmodel, epoch, w, world.config["multicore"])
-                print(test_res)
+                print("Test-Set",test_res)
                 break
 
         for i in range(len(world.valid_topks)):
@@ -166,7 +166,7 @@ print(
     )
 )
 if "NNI_PLATFORM" in os.environ:
-    metric = {"default": best_ndcg[0], "recall": best_recall[0], "hit": best_hit[0], "precision": best_precision[0]}
+    metric = {"ndcg": best_ndcg[0], "default": best_recall[0], "hit": best_hit[0], "precision": best_precision[0]}
     nni.report_final_result(metric)
 
 print("Total time:{}".format(time.time() - start_total))