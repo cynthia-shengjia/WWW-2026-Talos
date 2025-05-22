fa37ba2c8ac9ccd90c2368af57197484e34bcbf9
zhang shengjia <shengjia.zhang@zju.edu.cn>
2025-05-22 10:18:51 +0800

    fix: copyright

--- optimizer/optim_Base.py
+++ optimizer/optim_Base.py
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------
-# Copyright (c) 2025 Year Anonymous Authors of NeurIPS 2025 Submission 10239
+# Copyright (c) 2025 Anonymous Authors of NeurIPS 2025 Submission 10239
 # -------------------------------------------------------------------
 from abc import ABC, abstractmethod
 
